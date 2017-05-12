67d7ccf43da61bc9bf58ed7434ecb2f1391a0e9b
vianney <vianney@quasardb.net>
2017-05-12 12:48:06 +0200

    Moved alias generator to utils

--- utils.go
+++ utils.go
@@ -7,8 +7,34 @@ package qdb
 */
 import "C"
 import "unsafe"
+import "math/rand"
 
 // Release : release previously allocated qdb resource
 func (handle HandleType) Release(buffer unsafe.Pointer) {
 	C.qdb_release(handle.t, buffer)
+}
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func generateAlias(n int) string {
+	b := make([]byte, n)
+	// A rand.Int63() generates 63 random bits, enough for letterIdxMax letters!
+	for i, cache, remain := n-1, rand.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = rand.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
 }
