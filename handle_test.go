5652929a2718cf02f35a4b001a095974cbc944ee
vianney <vianney@quasardb.net>
2017-05-15 13:52:52 +0200

    Added names to tests

--- handle_test.go
+++ handle_test.go
@@ -5,22 +5,35 @@ import (
 	"testing"
 )
 
-// TestConnect testing various things about connection
-func TestHandle(t *testing.T) {
-	var handle HandleType
-	err := handle.Connect("")
+func connectWithoutCreatingHandle(t *testing.T, handle *HandleType, err error) {
+	err = handle.Connect("")
 	if err == nil {
 		t.Error("Expected error on: Connect without a proper handle - got nil")
 	}
-	handle, err = NewHandle()
+}
+
+func connectWithoutAddress(t *testing.T, handle *HandleType, err error) {
+	*handle, err = NewHandle()
 	err = handle.Connect("")
 	if err == nil {
 		t.Error("Expected error on: Connect without a proper address - got nil")
 	}
+}
+
+func connectProperly(t *testing.T, handle *HandleType, err error) {
 	qdbConnection := string("qdb://127.0.0.1:")
 	qdbConnection += os.Args[1]
 	err = handle.Connect(qdbConnection)
 	if err != nil {
 		t.Error("Expected no error, got ", err)
 	}
+}
+
+// TestHandle testing various things about connection
+func TestHandle(t *testing.T) {
+	var handle HandleType
+	var err error
+	connectWithoutCreatingHandle(t, &handle, err)
+	connectWithoutAddress(t, &handle, err)
+	connectProperly(t, &handle, err)
 }
