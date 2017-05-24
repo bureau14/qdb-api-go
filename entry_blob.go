4303cf648687794cf90f3ed092c23a502e01eb07
vianney <vianney@quasardb.net>
2017-05-24 14:32:12 +0200

    Fixed empty content

--- entry_blob.go
+++ entry_blob.go
@@ -19,8 +19,13 @@ type BlobEntry struct {
 // Put : blob put value for alias
 func (entry BlobEntry) Put(content []byte, expiry Expiry) error {
 	alias := C.CString(entry.alias)
-	contentPtr := unsafe.Pointer(&content[0])
 	contentSize := C.qdb_size_t(len(content))
+	var contentPtr unsafe.Pointer
+	if contentSize != 0 {
+		contentPtr = unsafe.Pointer(&content[0])
+	} else {
+		contentPtr = unsafe.Pointer(nil)
+	}
 	err := C.qdb_blob_put(entry.handle, alias, contentPtr, contentSize, C.qdb_time_t(expiry))
 	return makeErrorOrNil(err)
 }
@@ -28,9 +33,14 @@ func (entry BlobEntry) Put(content []byte, expiry Expiry) error {
 // Update : blob update value of alias
 func (entry *BlobEntry) Update(newContent []byte, newExpiry Expiry) error {
 	alias := C.CString(entry.alias)
-	newContentPtr := unsafe.Pointer(&newContent[0])
 	contentSize := C.qdb_size_t(len(newContent))
-	err := C.qdb_blob_update(entry.handle, alias, newContentPtr, contentSize, C.qdb_time_t(newExpiry))
+	var contentPtr unsafe.Pointer
+	if contentSize != 0 {
+		contentPtr = unsafe.Pointer(&newContent[0])
+	} else {
+		contentPtr = unsafe.Pointer(nil)
+	}
+	err := C.qdb_blob_update(entry.handle, alias, contentPtr, contentSize, C.qdb_time_t(newExpiry))
 	return makeErrorOrNil(err)
 }
 
