f00711770f246932ab741cb378d26d8ffe1a30e0
vianney <vianney@quasardb.net>
2017-06-06 11:22:29 +0200

    Added documentation examples

---
+++ examples_test.go
@@ -0,0 +1,41 @@
+package qdb
+
+import (
+	"fmt"
+)
+
+func ExampleHandleType() {
+	var handle HandleType
+	handle.Open(ProtocolTCP)
+	fmt.Printf("API build: %s\n", handle.APIVersion())
+	// Output: API build: 2.1.0master
+}
+
+func ExampleBlobEntry_Alias() {
+	handle, err := setupHandle()
+	if err != nil {
+		panic(err)
+	}
+	defer handle.Close()
+
+	alias := "EntryAlias"
+	blob := handle.Blob(alias)
+
+	fmt.Printf("Alias: %s\n", blob.Alias())
+	// Output: Alias: EntryAlias
+}
+
+func ExampleBlobEntry_Put() {
+	handle, err := setupHandle()
+	if err != nil {
+		panic(err)
+	}
+	defer handle.Close()
+
+	blob := handle.Blob("blob")
+	blob.Put([]byte("content"), NeverExpires)
+
+	content, err := blob.Get()
+	fmt.Printf("Content: %s\n", content)
+	// Output: Content: content
+}
