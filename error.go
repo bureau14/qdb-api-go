548d0147402c3f47a6cb56896838bfe8444c3c4f
vianney <vianney@quasardb.net>
2017-06-09 17:15:32 +0200

    ok_created is not an error

--- error.go
+++ error.go
@@ -11,7 +11,7 @@ type ErrorType C.qdb_error_t
 func (e ErrorType) Error() string { return C.GoString(C.qdb_error(C.qdb_error_t(e))) }
 
 func makeErrorOrNil(err C.qdb_error_t) error {
-	if err != 0 {
+	if err != 0 && err != C.qdb_e_ok_created {
 		return ErrorType(err)
 	}
 	return nil
