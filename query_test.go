4370d8d57a40fc9ff84e7bfaaf7a5c56f8817aa1
vianney <vianney.p@outlook.com>
2020-12-11 10:15:53 +0100

    QDB-3131 - Add query symbols.

--- query_test.go
+++ query_test.go
@@ -13,53 +13,60 @@ var _ = Describe("Tests", func() {
 	var (
 		alias string
 
-		timeseries      TimeseriesEntry
+		timeseries TimeseriesEntry
 
 		blobColumn      TsBlobColumn
 		doubleColumn    TsDoubleColumn
 		int64Column     TsInt64Column
-		stringColumn     TsStringColumn
+		stringColumn    TsStringColumn
 		timestampColumn TsTimestampColumn
+		symbolColumn    TsSymbolColumn
 		columnsInfo     []TsColumnInfo
-		
+
 		timestamps      []time.Time
 		blobPoints      []TsBlobPoint
 		doublePoints    []TsDoublePoint
 		int64Points     []TsInt64Point
 		stringPoints    []TsStringPoint
 		timestampPoints []TsTimestampPoint
+		symbolPoints    []TsSymbolPoint
 	)
 	const (
-		count	int64 = 8
-		start	int64 = 0
-		end		int64 = count - 1
+		count int64 = 8
+		start int64 = 0
+		end   int64 = count - 1
 
-		blobIndex		int64 = 0
-		doubleIndex		int64 = 1
-		int64Index		int64 = 2
-		stringIndex		int64 = 3
-		timestampIndex	int64 = 4
+		blobIndex      int64 = 0
+		doubleIndex    int64 = 1
+		int64Index     int64 = 2
+		stringIndex    int64 = 3
+		timestampIndex int64 = 4
+		symbolIndex    int64 = 5
 
 		blobColName      string = "blob_col"
-		stringColName     string = "string_col"
+		stringColName    string = "string_col"
 		doubleColName    string = "double_col"
 		int64ColName     string = "int64_col"
 		timestampColName string = "timestamp_col"
+		symbolColName    string = "symbol_col"
+
+		symtableName string = "symbol_table"
 	)
 	BeforeEach(func() {
 		alias = generateAlias(16)
 
 		columnsInfo = []TsColumnInfo{}
-		columnsInfo = append(columnsInfo, NewTsColumnInfo(blobColName, TsColumnBlob), NewTsColumnInfo(doubleColName, TsColumnDouble), NewTsColumnInfo(int64ColName, TsColumnInt64), NewTsColumnInfo(stringColName, TsColumnString), NewTsColumnInfo(timestampColName, TsColumnTimestamp))
-		
+		columnsInfo = append(columnsInfo, NewTsColumnInfo(blobColName, TsColumnBlob), NewTsColumnInfo(doubleColName, TsColumnDouble), NewTsColumnInfo(int64ColName, TsColumnInt64), NewTsColumnInfo(stringColName, TsColumnString), NewTsColumnInfo(timestampColName, TsColumnTimestamp), NewSymbolColumnInfo(symbolColName, symtableName))
+
 		timeseries = handle.Timeseries(alias)
-		
+
 		timestamps = make([]time.Time, count)
 		blobPoints = make([]TsBlobPoint, count)
 		doublePoints = make([]TsDoublePoint, count)
 		int64Points = make([]TsInt64Point, count)
 		stringPoints = make([]TsStringPoint, count)
 		timestampPoints = make([]TsTimestampPoint, count)
+		symbolPoints = make([]TsSymbolPoint, count)
 		for idx := int64(0); idx < count; idx++ {
 			timestamps[idx] = time.Unix((idx+1)*10, 0)
 			blobPoints[idx] = NewTsBlobPoint(timestamps[idx], []byte(fmt.Sprintf("content_%d", idx)))
@@ -67,9 +74,9 @@ var _ = Describe("Tests", func() {
 			int64Points[idx] = NewTsInt64Point(timestamps[idx], idx)
 			stringPoints[idx] = NewTsStringPoint(timestamps[idx], fmt.Sprintf("content_%d", idx))
 			timestampPoints[idx] = NewTsTimestampPoint(timestamps[idx], timestamps[idx])
+			symbolPoints[idx] = NewTsSymbolPoint(timestamps[idx], fmt.Sprintf("content_%d", idx))
 		}
 
-		
 		err := timeseries.Create(24*time.Hour, columnsInfo...)
 		Expect(err).ToNot(HaveOccurred())
 		blobColumn = timeseries.BlobColumn(columnsInfo[blobIndex].Name())
@@ -77,12 +84,14 @@ var _ = Describe("Tests", func() {
 		int64Column = timeseries.Int64Column(columnsInfo[int64Index].Name())
 		stringColumn = timeseries.StringColumn(columnsInfo[stringIndex].Name())
 		timestampColumn = timeseries.TimestampColumn(columnsInfo[timestampIndex].Name())
-		
+		symbolColumn = timeseries.SymbolColumn(columnsInfo[symbolIndex].Name(), symtableName)
+
 		blobColumn.Insert(blobPoints...)
 		doubleColumn.Insert(doublePoints...)
 		int64Column.Insert(int64Points...)
 		stringColumn.Insert(stringPoints...)
 		timestampColumn.Insert(timestampPoints...)
+		symbolColumn.Insert(symbolPoints...)
 	})
 	AfterEach(func() {
 		timeseries.Remove()
@@ -123,6 +132,10 @@ var _ = Describe("Tests", func() {
 				Expect(err).ToNot(HaveOccurred())
 				Expect(timestampValue).To(Equal(timestampPoints[rowIdx].Content()))
 
+				symbolValue, err := columns[symbolIndex+2].GetSymbol()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(symbolValue).To(Equal(symbolPoints[rowIdx].Content()))
+
 				for i, column := range result.Columns(row) {
 					if i == 1 {
 						// Skip $table
@@ -151,6 +164,10 @@ var _ = Describe("Tests", func() {
 						value := point.Value()
 						Expect(err).ToNot(HaveOccurred())
 						Expect(value).To(Equal(timestampPoints[rowIdx].Content()))
+					case QueryResultSymbol:
+						value := point.Value()
+						Expect(err).ToNot(HaveOccurred())
+						Expect(value).To(Equal(symbolPoints[rowIdx].Content()))
 					}
 				}
 			}
