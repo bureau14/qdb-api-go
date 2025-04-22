f426dcb6171578c07deddee8cf9d64e7dab37a3d
Leon Mergen <leon@solatis.com>
2025-04-22 11:33:34 +0700

    Fix permissions issue

--- scripts/teamcity/10.build.sh
+++ scripts/teamcity/10.build.sh
@@ -5,4 +5,7 @@ set -eu
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
+# Fix permission issue when using docker builds
+git config --global --add safe.directory '*'
+
 ${GO} build -v -x
