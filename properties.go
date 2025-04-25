2476df0d115f0f5e4134f159273f5b9821023e54
Aleksey Alekseev <voodoo144@gmail.com>
2025-04-24 22:05:58 -0400

    SC-15861: Draft properties tests

--- properties.go
+++ properties.go
@@ -1,1 +1,34 @@
 package qdb
+
+/*
+	#include <qdb/properties.h>
+*/
+import "C"
+import "unsafe"
+
+func (h HandleType) PutProperties(prop string, value string) error {
+	err := C.qdb_user_properties_put(h.handle, C.CString(prop), C.CString(value))
+	return ErrorType(err)
+}
+
+func (h HandleType) UpdateProperties(prop string, value string) error {
+	err := C.qdb_user_properties_update(h.handle, C.CString(prop), C.CString(value))
+	return ErrorType(err)
+}
+
+func (h HandleType) GetProperties(prop string) (string, error) {
+	var value *C.char
+	defer h.handle.Release(unsafe.Pointer(value))
+	err := C.qdb_user_properties_get(h.handle, C.CString(prop), &value)
+	return C.GoStringN(value), ErrorType(err)
+}
+
+func (h HandleType) RemoveProperties(prop string) error {
+	err := C.qdb_user_properties_remove(h.handle, C.CString(prop))
+	return ErrorType(err)
+}
+
+func (h HandleType) RemoveAllProperties() error {
+	err := C.qdb_user_properties_remove_all(h.handle)
+	return ErrorType(err)
+}
--- properties_test.go
+++ properties_test.go
@@ -1,1 +1,76 @@
 package qdb
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Tests", func() {
+	Context("Properties", func() {
+		var (
+			prop  string
+			value string
+		)
+		BeforeEach(func() {
+			prop = generateAlias(16)
+			value = generateAlias(16)
+		})
+
+		It("should have system properties after connect", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not be able to update system properties", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not be able to put empty property, non empty value", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not be able to put duplicate", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not be able to put empty property, empty value", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not be able to put empty value, non-empty property", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+		It("should put non-empty properties, non-empty value", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should get properties", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not get properties by empty name", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should remove properties", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not remove properties with empty name", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should not be able to remove system properties", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should remove all properties, but keep system", func() {
+			err := handle.Connect(insecureURI)
+			Expect(err).To(HaveOccurred())
+		})
+		It("should be able to update property", func() {
+			value, err := handle.GetProperties("test")
+			Expect(err).To(HaveOccurred())
+		})
+	})
+})
