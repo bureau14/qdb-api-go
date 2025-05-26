21f869c53bca641f322a49a065b151907b8d6631
Leon Mergen <leon@solatis.com>
2025-05-26 20:03:49 +0700

    Add some metadata here

--- scripts/codex/setup.sh
+++ scripts/codex/setup.sh
@@ -10,8 +10,13 @@ mkdir qdb/
 
 pushd qdb
 
+echo "Downloading C API"
 curl -s -L https://download.quasar.ai/quasardb/3.14/3.14.1/api/c/qdb-3.14.1-linux-64bit-c-api.tar.gz | tar -xzf -
+
+echo "Downloading server"
 curl -s -L https://download.quasar.ai/quasardb/3.14/3.14.1/server/qdb-3.14.1-linux-64bit-server.tar.gz | tar -xzf -
+
+echo "Downloading utils"
 curl -s -L https://download.quasar.ai/quasardb/3.14/3.14.1/utils/qdb-3.14.1-linux-64bit-utils.tar.gz | tar -xzf -
 
 popd
