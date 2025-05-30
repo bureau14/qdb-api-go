672d5ddeb97d6a588e833cca849febda6b949fd3
Leon Mergen <leon@solatis.com>
2025-05-30 09:54:33 +0700

    First shot at bulk reader implementation

---
+++ reader.go
@@ -0,0 +1,53 @@
+// Package qdb provides an api to a quasardb server
+package qdb
+
+/*
+        #include <string.h> // for memcpy
+	#include <qdb/client.h>
+	#include <qdb/ts.h>
+*/
+import "C"
+import "fmt"
+
+type ReaderData interface {
+	// Returns the column name
+	Name() string
+
+	// Possibly some methods, but often empty
+	valueType() TsValueType
+
+	// Release any C allocated buffers managed
+	release(h HandleType) error
+}
+
+// Int64
+type ReaderDataInt64 struct {
+	name string
+	xs   []int64
+}
+
+// Internal function used to convert C.qdb_exp_batch_push_column_t to Go. Memory-safe function
+// that copies data.
+//
+// Assumes `data.data_type` is int64,returns error otherwise.
+//
+// name: column name
+// xs:   C array of reader column data
+// n:    length of `data` inside array
+func newReaderDataInt64(name string, xs C.qdb_exp_batch_push_column_t, n int) (ReaderDataInt64, error) {
+	// Step 1: validate xs is of type qdb_ts_column_int64, yield error otherwise
+	if xs.data_type != C.qdb_ts_column_int64 {
+		return ReaderDataInt64{}, fmt.Errorf("Internal error, expected data type to be int64, got: %v", xs.data_type)
+	}
+
+	// Step 2: do a cast of xs.data[0] to *C.qdb_int_t. As pointer sizes on different architectures
+	//         may differ, *cannot* assume it's 8 bytes, and instead use `unsafe.Pointer` as the
+	//         architecture-safe representation for the size.
+
+	// Step 3: copy data by first interpreting it as a temporary memory unsafe-slice, then copying
+	//         it into a new, entirely Go-managed slice. This ensures the data can live on even when
+	//         we move to a new "batch" of reader data.
+
+	// Return result
+	return ReaderDataInt64{}, nil
+}
