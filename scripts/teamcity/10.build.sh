abcb1bb355f8386a84c06c3d2c11c673bacdde17
Leon Mergen <leon@solatis.com>
2025-06-18 18:44:12 +0700

    Revert "QDB-15861 - User properties support (#48)"
    This reverts commit 91ca48d761aae8fa0b0b98fd9d7186e5a82d4071.

--- scripts/teamcity/10.build.sh
+++ scripts/teamcity/10.build.sh
@@ -4,7 +4,6 @@ set -eu
 
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/common.sh"
-$SCRIPT_DIR/set_version.sh
 
 # Fix permission issue when using docker builds
 git config --global --add safe.directory '*'
