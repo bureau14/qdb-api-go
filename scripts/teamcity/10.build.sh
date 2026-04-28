400a66e3620db159f5c85ffe4ce72777daef24f9
Igor <igor.niebylski@gmail.com>
2026-04-28 13:46:07 +0200

    less debug

--- scripts/teamcity/10.build.sh
+++ scripts/teamcity/10.build.sh
@@ -6,8 +6,6 @@ set -eu
 echo "Environment variables:"
 echo "GOROOT: ${GOROOT}"
 echo "GOPATH: ${GOPATH}"
-echo "QDB_CICD_AGENT_GO123_GOPATH: ${QDB_CICD_AGENT_GO123_GOPATH}"
-echo "QDB_CICD_AGENT_GO124_GOPATH: ${QDB_CICD_AGENT_GO124_GOPATH}"
 
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/common.sh"
