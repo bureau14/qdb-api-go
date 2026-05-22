4a0b3e573e1624ff20269f5166b2aae740622a98
Igor <73477532+igorniebylski@users.noreply.github.com>
2026-05-22 10:33:42 +0200

    QDB-19082 - [buildkite] Add test report plugin to Go API pipeline (#106)

--- scripts/teamcity/20.test.sh
+++ scripts/teamcity/20.test.sh
@@ -5,4 +5,5 @@ set -eu
 SCRIPT_DIR="$(cd "$(dirname -- "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
-${GO} test -v -json -test.run "Test*" -coverprofile=test-coverage.out
+# Convert test output to JUnit XML so buildkite can understand it
+${GO} test -v -test.run "Test*" -coverprofile=test-coverage.out | ${GO_JUNIT_REPORT} -out "${TEST_REPORT_DIR}/junit-test-report.xml" -iocopy
