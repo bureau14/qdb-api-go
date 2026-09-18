e4d115d8fe7e29733c137435187ed3acb832ff1a
Leon Mergen <leon@solatis.com>
2026-09-18 17:16:03 +0800

    test(time): cover NullTime conversion

--- time_test.go
+++ time_test.go
@@ -1,6 +1,7 @@
 package qdb
 
 import (
+	"math"
 	"testing"
 	"time"
 
@@ -27,4 +28,32 @@ func TestTimeCanConvertToQdbTimespec(t *testing.T) {
 
 		assert.Equal(input, output)
 	})
+}
+
+// A null in a timestamp column is qdb_min_time in both timespec fields, which
+// time.Time normalises away; NullTime has to map onto it and back.
+func TestNullTimeConvertsToNullTimespec(t *testing.T) {
+	assert := assert.New(t)
+
+	input := []time.Time{time.Unix(1, 2).UTC(), NullTime(), time.Time{}}
+	native := TimeSliceToQdbTimespec(input)
+
+	assert.False(isNullTimespec(native[0]))
+	assert.True(isNullTimespec(native[1]))
+	assert.False(isNullTimespec(native[2]), "the zero time is a value, not a null")
+
+	output := QdbTimespecSliceToTime(native)
+	assert.Equal(input[0], output[0])
+	assert.True(IsNullTime(output[1]))
+	assert.Equal(NullTime(), output[1])
+}
+
+func TestIsNullTime(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.True(IsNullTime(NullTime()))
+	assert.True(IsNullTime(NullTime().Local()))
+	assert.True(IsNullTime(time.Unix(0, math.MinInt64)))
+	assert.False(IsNullTime(time.Time{}))
+	assert.False(IsNullTime(NullTime().Add(time.Nanosecond)))
 }
